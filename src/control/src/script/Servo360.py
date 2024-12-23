6ccaac3e4fdee40a17b82d2530cced603405ae45
AMansour5 <ahmedmonsour5@icloud.com>
2024-12-23 15:04:50 +0000

    To be tested

---
+++ src/control/src/script/Servo360.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+from zope.interface import implementer
+from interface.Servo360Interface import IServo360
+from interface.PWMDriver import PWMDriver
+import time
+@implementer(IServo360)
+class Servo360:
+    """
+    360Servo class to control the rotation angle of 360servo moves in range of (1 - 2 ms)
+    ms > 1.5 --> counter clockwise 
+    ms < 1.5 --> clockwise 
+    ms = 1.5 stop 
+    """
+    def __init__(self, channel: int):
+        self.__pwm_driver = PWMDriver()
+        if not 0 <= channel <= 15:
+            raise ValueError("Channel must be between 0 and 15.")
+        self.__channel = channel
+        self.__forward_value = self.__pwm_driver.microsecondsToDutycycle(1495) 
+        self.__stop_value = self.__pwm_driver.microsecondsToDutycycle(1500)
+        self.__backward_value = self.__pwm_driver.microsecondsToDutycycle(1505)  
+
+    def goForward(self) -> None:
+        """
+        Makes the servo move clockwise with a very small angle
+        :param channel: the channel the servo is connected to.
+        """
+        self.__pwm_driver.PWMWrite(self.__channel, self.__forward_value)
+        time.sleep(0.0001)
+        self.__pwm_driver.PWMWrite(self.__channel, self.__stop_value)
+        
+
+    def goBackwards(self) -> None:
+        """
+        Makes the servo move counter clockwise with a very small angle
+        :param channel: the channel the servo is connected to.
+        """
+        self.__pwm_driver.PWMWrite(self.__channel, self.__backward_value)
+        time.sleep(0.0001)
+        self.__pwm_driver.PWMWrite(self.__channel, self.__stop_value)
+
+    def Stop(self) -> None:
+        """
+        Makes the servo stop the motion
+        :param channel: the channel the servo is connected to.
+        """
+        self.__pwm_driver.PWMWrite(self.__channel, self.__stop_value)
+    
