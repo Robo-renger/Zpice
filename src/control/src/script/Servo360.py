3c519ccfe88f4dd60a7d29ee9c6ebd9e9123e2a2
AMansour5 <ahmedmonsour5@icloud.com>
2025-01-26 13:47:44 +0200

    Servo360done + servo180 software test script remaining

--- src/control/src/script/Servo360.py
+++ src/control/src/script/Servo360.py
@@ -3,42 +3,41 @@
 from zope.interface import implementer
 from interface.Servo360Interface import IServo360
 from interface.PWMDriver import PWMDriver
-from script.services.PCADriver import PCA
 import time
 @implementer(IServo360)
 class Servo360:
     """
-    360Servo class to control the rotation angle of 360servo moves in range of (1 - 2 ms)
-    ms > 1.5 --> counter clockwise 
-    ms < 1.5 --> clockwise 
-    ms = 1.5 stop 
+    360Servo class to control the rotation angle of 360servo moves in range of (1000 - 2000 us)
+    ms > 1500 --> counter clockwise 
+    ms < 1500 --> clockwise 
+    ms = 1500 stop 
     """
     def __init__(self, channel: int, pwm_driver: PWMDriver):
         if not 0 <= channel <= 15:
             raise ValueError("Channel must be between 0 and 15.")
         self.__pwm_driver = pwm_driver
         self.__channel = channel
-        self.__forward_value = self.__pwm_driver.microsecondsToDutycycle(1495) 
-        self.__stop_value = self.__pwm_driver.microsecondsToDutycycle(1500)
-        self.__backward_value = self.__pwm_driver.microsecondsToDutycycle(1505)  
+        self.__forward_value = 1495
+        self.__stop_value = 1500
+        self.__backward_value = 1505  
 
-    def goForward(self) -> None:
+    def goForward(self, delay: int = 0.0001) -> None:
         """
         Makes the servo move clockwise with a very small angle
         :param channel: the channel the servo is connected to.
         """
         self.__pwm_driver.PWMWrite(self.__channel, self.__forward_value)
-        time.sleep(0.0001)
+        time.sleep(delay)
         self.Stop()
         
 
-    def goBackwards(self) -> None:
+    def goBackwards(self, delay: int = 0.0001) -> None:
         """
         Makes the servo move counter clockwise with a very small angle
         :param channel: the channel the servo is connected to.
         """
         self.__pwm_driver.PWMWrite(self.__channel, self.__backward_value)
-        time.sleep(0.0001)
+        time.sleep(delay)
         self.Stop()
 
     def Stop(self) -> None:
