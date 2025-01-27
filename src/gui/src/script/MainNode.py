cc809fd511e6d1956ae69811d6637c3020e6be05
AMansour5 <ahmedmonsour5@icloud.com>
2025-01-27 19:10:35 +0000

    cameras working low res

--- src/gui/src/script/MainNode.py
+++ src/gui/src/script/MainNode.py
@@ -19,8 +19,8 @@ class CameraStreamerNode:
 
     def runStreams(self):
         for camera, details in self.camerasDetails.items():
-            html_content = GUIPresistence("/home/ziad/zpice_ws/src/gui/src/index.html").getGUI()
-            cameraStreamer = CameraStreamer("test.mp4",html_content)
+            html_content = GUIPresistence("/home/mypi/Zpice/src/gui/src/index.html").getGUI()
+            cameraStreamer = CameraStreamer(details['index'],html_content)
             self.cameraStreamers.append(cameraStreamer)
             cameraStreamer.setFPS(details['fps'])
             cameraStreamer.setFrameSize(details['width'], details['height'])
