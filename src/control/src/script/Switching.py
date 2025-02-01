44254a9c480d8f12052e84aee6d5c74f71e97c99
MahmoudTarek02 <mahmoudtarek152@gmail.com>
2025-02-01 13:15:34 +0200

    before warsha

--- src/control/src/script/Switching.py
+++ src/control/src/script/Switching.py
@@ -1,11 +1,16 @@
-from zope.interface import implementer
-from iSwitching import iSwitching
+#!/usr/bin/env python3
 
+from zope.interface import implementer
+from interface.iSwitching import iSwitching
+
+SIMULATION_MODE = False
 try:
     import RPi.GPIO as GPIO
-    SIMULATION_MODE = False
-except ImportError:
+    
+except (RuntimeError, ImportError):
     SIMULATION_MODE = True
+    print("RPi.GPIO not found. Running in simulation mode.")
+    
 
 @implementer(iSwitching)
 class Switching:
@@ -15,13 +20,16 @@ class Switching:
         gpio_interface used for mocking the GPIO library.
         """
         self.simulation_mode = SIMULATION_MODE
-        self.gpio = gpio_interface if gpio_interface else (GPIO if not self.simulation_mode else None)
+        # self.gpio = gpio_interface if gpio_interface else (GPIO if not self.simulation_mode else None)
         if not self.simulation_mode:
+            self.gpio = GPIO
             self.gpio.setmode(GPIO.BCM)
             self.gpio.setup(pin, GPIO.OUT)
             self.gpio.output(pin, GPIO.LOW) # Initialize the pin to low
+        else:
+            self.gpio = None
         self.pin = pin
-        self.is_open = False
+        self._is_open = False
         
     def open(self) -> None:
         if not self.simulation_mode:
--- src/control/src/script/Thruster.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-# from control.src.services.PWMMotor import PWM_Motors
-# from PWMMotor import PWMMotor
-from PWM_Motor import PWMMotor
-
-class Thruster(PWMMotor):
-    def __init__(self, pca, channel, min_value=1100, max_val=1900, init_value=1500, smoothing_strategy=None):
-        super().__init__(pca, channel, min_value, max_val, init_value, smoothing_strategy)
