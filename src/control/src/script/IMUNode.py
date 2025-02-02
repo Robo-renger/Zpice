06dd4c0f68785abcc9833c3a906f13dd6e7eb10f
AMansour5 <ahmedmonsour5@icloud.com>
2025-02-02 13:27:31 +0200

    IMUNode

---
+++ src/control/src/script/IMUNode.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import rospy
+import board
+import time
+import adafruit_bno08x as bno
+from services.IMU import BNO085
+from control.msg import IMU
+from digitalio import DigitalInOut
+
+class IMUNode:
+    def __init__(self) -> None:
+        rospy.init_node("IMUNode")
+        self.pub = rospy.Publisher("IMU", IMU, queue_size=10)
+        self.msg = IMU()
+        self.imu = BNO085()
+
+    def calibrate(self) -> None:
+        self.imu.Calibrate()
+
+    def enableFeature(self, feature: int) -> None:
+        self.imu.enableFeature(feature)
+
+    def run(self):
+        roll, pitch, yaw = self.imu.getEulerAngles()
+        self.msg.roll = roll
+        self.msg.pitch = pitch 
+        self.msg.yaw = yaw
+        self.pub.publish()
+
+if __name__ == "__main__":
+    try:
+        mode = input("Enter mode: c -> Calibration, r -> Read data: ")
+        if mode == "c":
+            reset_pin = DigitalInOut(board.D5)
+            imu = IMUNode(reset_pin, True)
+            imu.Calibrate()
+        else:
+            imu = IMUNode()
+            imu.enableFeature(bno.BNO_REPORT_ACCELEROMETER)
+            imu.enableFeature(bno.BNO_REPORT_GYROSCOPE)
+            imu.enableFeature(bno.BNO_REPORT_MAGNETOMETER)
+            imu.enableFeature(bno.BNO_REPORT_ROTATION_VECTOR)
+            while not rospy.is_shutdown():
+                imu.run()
+
+    except KeyboardInterrupt:
+        print("Stopping...")
+
+
--- src/control/src/script/services/IMU.py
+++ src/control/src/script/services/IMU.py
@@ -1,40 +1,68 @@
 #!/usr/bin/env python3
 import time
-import board
 import math
+import adafruit_bno08x as bno
+from interface.IBNO085 import IBNO085
+from zope.interface import implementer
 from digitalio import DigitalInOut
-from adafruit_bno08x import BNO_REPORT_ACCELEROMETER, BNO_REPORT_GYROSCOPE, BNO_REPORT_MAGNETOMETER, BNO_REPORT_ROTATION_VECTOR, REPORT_ACCURACY_STATUS
 from adafruit_bno08x.i2c import BNO08X_I2C
 from adafruit_extended_bus import ExtendedI2C
 
+@implementer(IBNO085)
 class BNO085:
-    def __init__(self, reset_pin: DigitalInOut = None, debug: bool =False):
+    def __init__(self, address: int = 0x4b, reset_pin: DigitalInOut = None, debug: bool =False):
         i2c = ExtendedI2C(3, frequency=400000)
-        self.reset_pin = reset_pin
-        self.bno = BNO08X_I2C(i2c, address=0x4b, reset=self.reset_pin, debug=debug)
+        self.bno = BNO08X_I2C(i2c, address=address, reset=reset_pin, debug=debug)
 
-    def enableFeature(self, feature):
+    def enableFeature(self, feature: int) -> None:
         self.bno.enable_feature(feature)
 
-    def Calibrate(self):
+    def getAcceleration(self) -> tuple:
+        accel_x, accel_y, accel_z = self.bno.acceleration  
+        return accel_x, accel_y, accel_z
+
+    def getGyro(self) -> tuple:
+        gyro_x, gyro_y, gyro_z = self.bno.gyro 
+        return gyro_x, gyro_y, gyro_z
+
+    def getMagnetometer(self) -> tuple:
+        mag_x, mag_y, mag_z = self.bno.magnetic 
+        return mag_x, mag_y, mag_z
+
+    def getQuaternion(self) -> tuple:
+        quat_i, quat_j, quat_k, quat_real = self.bno.quaternion 
+        return quat_i, quat_j, quat_k, quat_real
+    
+    def getEulerAngles(self) -> tuple:
+        q1, q2, q3, q0 = self.bno.quaternion
+        yaw = math.atan2(2 * (q0 * q3 + q1 * q2), 1 - 2 * (q2 * q2 + q3 * q3))
+        pitch = math.asin(2 * (q0 * q2 - q3 * q1))
+        roll = math.atan2(2 * (q0 * q1 + q2 * q3), 1 - 2 * (q1 * q1 + q2 * q2))
+        return roll, pitch, yaw
+    
+
+
+    def Calibrate(self) -> None:
+        # Needs more testing and improvements
         print("Starting calibration")
         self.bno.begin_calibration()
         print("Calibration started...\n Enabling calibration features...")
-        self.enableFeature(BNO_REPORT_MAGNETOMETER)
-        self.enableFeature(BNO_REPORT_ROTATION_VECTOR)
+        self.enableFeature(bno.BNO_REPORT_MAGNETOMETER)
+        self.enableFeature(bno.BNO_REPORT_GAME_ROTATION_VECTOR)
         start_time = time.monotonic()
         calibration_good_at = None
         while True:
             time.sleep(0.1)
-            self.getMagnetometer()
-            self.getQuaternion()
+            print("Magnetometer:")
+            mag_x, mag_y, mag_z = self.bno.magnetic
+            print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z))
+            print("")
+            print("Game Rotation Vector Quaternion:")
+            (game_quat_i, game_quat_j, game_quat_k, game_quat_real) = self.bno.game_quaternion  
+            print("I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (game_quat_i, game_quat_j, game_quat_k, game_quat_real))
             print("********************************")
             calibration_status = self.bno.calibration_status
-            print(
-                "Magnetometer Calibration quality:",
-                REPORT_ACCURACY_STATUS[calibration_status],
-                " (%d)" % calibration_status,
-            )
+            print("Magnetometer Calibration quality:", bno.REPORT_ACCURACY_STATUS[calibration_status], " (%d)" % calibration_status)
             if not calibration_good_at and calibration_status >= 2:
                 calibration_good_at = time.monotonic()
             if calibration_good_at and (time.monotonic() - calibration_good_at > 5.0):
@@ -44,76 +72,4 @@ class BNO085:
                     break
                 calibration_good_at = None
             print("**************************************************************")
-
         print("calibration done")
-
-    def getAcceleration(self):
-        print("Acceleration:")
-        accel_x, accel_y, accel_z = self.bno.acceleration  
-        print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
-        print("")
-        return accel_x, accel_y, accel_z
-
-    def getGyro(self):
-        print("Gyro:")
-        gyro_x, gyro_y, gyro_z = self.bno.gyro 
-        print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z))
-        print("")
-        return gyro_x, gyro_y, gyro_z
-
-    def getMagnetometer(self):
-        print("Magnetometer:")
-        mag_x, mag_y, mag_z = self.bno.magnetic 
-        print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z))
-        print("")
-        return mag_x, mag_y, mag_z
-
-    def getQuaternion(self):
-        print("Rotation Vector Quaternion:")
-        quat_i, quat_j, quat_k, quat_real = self.bno.quaternion 
-        print(
-            "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real)
-        )
-        print("")
-        return quat_i, quat_j, quat_k, quat_real
-    
-    def getYaw(self):
-        # print("Yaw:")
-        q1, q2, q3, q0 = self.bno.quaternion
-        yaw = math.atan2(2 * (q0 * q3 + q1 * q2), 1 - 2 * (q2 * q2 + q3 * q3))
-        print("Yaw: %0.6f" % math.degrees(yaw))
-        return yaw
-
-    def getPitch(self):
-        # print("Pitch:")
-        q1, q2, q3, q0 = self.bno.quaternion
-        pitch = math.asin(2 * (q0 * q2 - q3 * q1))
-        print("Pitch: %0.6f" % math.degrees(pitch))
-        return pitch
-
-
-    
-
-if __name__ == "__main__":
-    try:
-        mode = input("Enter mode: c -> Calibration, r -> Read data: ")
-        if mode == "c":
-            reset_pin = DigitalInOut(board.D5)
-            imu = BNO085(reset_pin, True)
-            imu.Calibrate()
-        else:
-            imu = BNO085()
-            imu.enableFeature(BNO_REPORT_ACCELEROMETER)
-            imu.enableFeature(BNO_REPORT_GYROSCOPE)
-            imu.enableFeature(BNO_REPORT_MAGNETOMETER)
-            imu.enableFeature(BNO_REPORT_ROTATION_VECTOR)
-            while True:
-                time.sleep(0.5)
-                # imu.getAcceleration()
-                # imu.getGyro()
-                # imu.getMagnetometer()
-                # imu.getQuaternion()
-                imu.getYaw()
-                imu.getPitch()
-    except KeyboardInterrupt:
-        print("Stopping...")
