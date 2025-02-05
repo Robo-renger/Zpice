35138e082cbb461f03667de488abfce32ac95510
Roborenger22 <roborenger72@gmail.com>
2025-02-05 03:59:49 +0200

    Changed the Thrustuer max value to 1823

--- src/control/src/services/Thruster.py
+++ src/control/src/services/Thruster.py
@@ -1,5 +1,6 @@
 from control.src.services.PWMMotor import PWM_Motors
 
 class Thruster(PWM_Motors):
-    def __init__(self, pca, channel, min_value=1100, max_val=1900, init_value=1500):
+    #Note: Max value was changed from 1900 to 1823 as this is equivelant to thruster max reverse direction PWM
+    def __init__(self, pca, channel, min_value=1100, max_val=1823, init_value=1500):
         super().__init__(pca, channel, min_value, max_val, init_value)
