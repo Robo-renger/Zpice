78607a928ad20e0c4868cb3f72799fe55892b6e3
AMansour5 <ahmedmonsour5@icloud.com>
2025-02-10 14:37:43 +0200

    navigationNode

--- src/control/src/script/NavigationNode.py
+++ src/control/src/script/NavigationNode.py
@@ -1,33 +1,22 @@
 #!/usr/bin/env python3
 
 import rospy
-import time
 from services.Navigation import Navigation
-from services.Vectorizer import Vectorizer
-from control.msg import Joystick
 from services.Joystick import CJoystick
 
 
 class NavigationNode:
     def __init__(self):
         rospy.init_node('navigation_node', anonymous=False)
-        joystick = CJoystick()
+        self.joystick = CJoystick()
 
-    def run(self):
-        rospy.Subscriber("/joystick", Joystick, self.navigate)
-        rospy.spin()
-
-    def navigate(self, data: Joystick):
-        x_axis = data.left_x_axis
-        y_axis = 0
-        z_axis = data.left_y_axis
-        yaw_axis = data.right_x_axis
-        pitch_axis = data.right_y_axis 
-        Navigation().navigate(x_axis, y_axis, z_axis, y_axis, pitch_axis)
+    def navigate(self):
+        left_x, left_y, right_x, right_y = self.joystick.getAxis()
+        Navigation().navigate(left_x, 0, left_y, right_y, right_x)
 
 if __name__ == "__main__":
     try:
         node = NavigationNode()
-        node.run()
+        node.navigate()
     except KeyboardInterrupt:
         rospy.loginfo("Exiting...")
