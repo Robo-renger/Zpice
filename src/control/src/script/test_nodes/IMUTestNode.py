e417f946848e784a349c66e65a1b3ffe3f562789
AMansour5 <ahmedmonsour5@icloud.com>
2025-02-18 02:28:45 +0200

    IMU node updated

--- src/control/src/script/test_nodes/IMUTestNode.py
+++ src/control/src/script/test_nodes/IMUTestNode.py
@@ -11,11 +11,12 @@ from exceptions.SensorReadError import SensorReadError
 from exceptions.SensorCalibrationError import SensorCalibrationError
 
 class IMUNode:
-    def __init__(self) -> None:
-        rospy.init_node("IMUNode")
+    def __init__(self, reset_pin = None) -> None:
+        rospy.init_node("imu_test_node")
         self.imu = self.initialize_sensor_with_retry()
         self.pub = rospy.Publisher("IMU", IMU, queue_size=10)
         self.msg = IMU()
+        self.reset_pin = reset_pin
 
     def initialize_sensor_with_retry(self) -> BNO085:
         """
@@ -29,13 +30,19 @@ class IMUNode:
         max_attempts = 5 
         for attempt in range(max_attempts):
             try:
-                imu = BNO085()
+                imu = BNO085(reset_pin=self.reset_pin)
                 return imu
             except SensorInitializationError as e:
                 if attempt < max_attempts - 1:
                     time.sleep(1)
                 else:
                     raise SensorInitializationError("IMU sensor initialization failed after all retries.")
+
+    def calibrate(self) -> None:
+        try:
+            self.imu.Calibrate()
+        except SensorCalibrationError as e:
+            rospy.logerr(f"{e} Skipping this cycle...")
 
     def enableFeature(self, feature: int) -> None:
         self.imu.enableFeature(feature)
@@ -47,18 +54,25 @@ class IMUNode:
             self.msg.pitch = pitch 
             self.msg.yaw = yaw
             self.pub.publish(self.msg)
+            rospy.loginfo(f"yaw: {yaw}, pitch: {pitch}")
         except SensorReadError as e:
             rospy.logerr(f"{e} Skipping this cycle...")
 
 if __name__ == "__main__":
     try:
-        imu = IMUNode()
-        imu.enableFeature(bno.BNO_REPORT_ACCELEROMETER)
-        imu.enableFeature(bno.BNO_REPORT_GYROSCOPE)
-        imu.enableFeature(bno.BNO_REPORT_MAGNETOMETER)
-        imu.enableFeature(bno.BNO_REPORT_ROTATION_VECTOR)
-        while not rospy.is_shutdown():
-            imu.run()
+        mode = input("Enter mode: c -> Calibration, r -> Read data: ")
+        if mode == "c":
+            reset_pin = DigitalInOut(board.D5)
+            imu = IMUNode(reset_pin, True)
+            imu.calibrate()
+        else:
+            imu = IMUNode()
+            imu.enableFeature(bno.BNO_REPORT_ACCELEROMETER)
+            imu.enableFeature(bno.BNO_REPORT_GYROSCOPE)
+            imu.enableFeature(bno.BNO_REPORT_MAGNETOMETER)
+            imu.enableFeature(bno.BNO_REPORT_ROTATION_VECTOR)
+            while not rospy.is_shutdown():
+                imu.run()
     except SensorInitializationError as e:
         rospy.logerr(f"Sensor initialization failed. {e}")
     except KeyboardInterrupt:
