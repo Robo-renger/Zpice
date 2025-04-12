eba8bdae32b7e3fc34d2ead5765ceff1d4c9db05
Amansour5 <Amansour5@github.com>
2025-04-12 15:55:34 +0000

    Photosphere tested

--- src/cv/setup.py
+++ src/cv/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # This setup is used to make (test, script, mock and ms5837) visible for each other
 setup(
     name='cv',  # Replace with your package name
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(where='src'),  # Automatically find packages under 'src'
     package_dir={'': 'src'},  # Specify that packages are located in 'src'
     install_requires=[],  # Add any Python dependencies here
