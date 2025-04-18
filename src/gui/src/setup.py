c5b9749ffa599d48e8eae6f8edf2e48acf36d833
Amansour5 <Amansour5@github.com>
2025-04-18 14:05:20 +0000

    mj

--- src/gui/src/setup.py
+++ src/gui/src/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # this setup is used to make all pakcages inside script directory visible for each other
 setup(
     name="gui2",         
-    version="0.0.21",
+    version="0.0.22",
     packages=find_packages(where="script"),  
     package_dir={"": "script"},   
     install_requires=[
