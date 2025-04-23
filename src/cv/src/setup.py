193a7f2df7f5c67133bfe2318da4fef6bab921bf
Amansour5 <Amansour5@github.com>
2025-04-23 15:31:34 +0000

    services run correctly

--- src/cv/src/setup.py
+++ src/cv/src/setup.py
@@ -3,11 +3,7 @@ from setuptools import setup, find_packages
 # this setup is used to make all pakcages inside script directory visible for each other
 setup(
     name="cv2",         
-<<<<<<< HEAD
-    version="0.0.25",
-=======
-    version="0.0.37",
->>>>>>> e6cfed3dd26a98c416fd417fc53ecaad6592ec9a
+    version="0.0.27",
     packages=find_packages(where="script"),  
     package_dir={"": "script"},   
     # zip_safe=True,
